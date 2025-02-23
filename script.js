b2328d02f5f3a54db38989ab4130e401428307f0
proProgrammer0101 <mannomuntah@gmail.com>
2025-02-23 00:40:49 -0800

    Adding some normal evilness with the calculator body

--- script.js
+++ script.js
@@ -23,15 +23,28 @@ function clearDisplay() {
     display.value = '';
 }
 
-// Evil Sounds here
+// Evil Sounds heree
 
 const calculatorBody = document.querySelector('.calculator');
 const kiras_madness = new Audio("kiras_madness.mp3");
 const soundButton = document.querySelector('.sound-btn a');
 
 soundButton.addEventListener('click', (event) => {
-    event.preventDefault();
+    // event.preventDefault();
+    alert('You are going to make a great mistake! 😈');
     kiras_madness.play();
     soundButton.classList.add('disabled');
-    calculatorBody.classList.add('animated-shadow');
+    calculatorBody.classList.toggle('animated-evilness');
+    mainEvilness();
 });
+
+function mainEvilness() {
+    const e1 = document.querySelectorAll('.e1');
+    const e2 = document.querySelectorAll('.e2');
+    const e3 = document.querySelectorAll('.e3');
+    const e4 = document.querySelectorAll('.e4');
+    const e5 = document.querySelectorAll('.e5');
+    const e6 = document.querySelectorAll('.e6');
+    const e7 = document.querySelectorAll('.e7');
+    const e8 = document.querySelectorAll('.e8');
+}
